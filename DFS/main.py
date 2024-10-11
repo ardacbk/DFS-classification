189318751021468933e452cdca0fefd1bdb0c5b8
ardacbk <ardacabuk1@hotmail.com>
2024-10-11 20:02:44 +0300

    Basic drawing added

--- DFS/main.py
+++ DFS/main.py
@@ -1,14 +1,27 @@
+import pygame
 from graph import Graph
 from node import Node
 
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RADIUS = 30
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
 graph = Graph()
-nodes = []
+nodes = [Node(1,120,100,(2,3))]
 
-for i in range(11):
-    nodes.append(Node(i,100+i*10,100+i*3,[i+1,i-1]))
-    graph.add_node(nodes[i])
+running = True
+while running:
+    for node in nodes:
+        pygame.draw.circle(screen,WHITE,(node.pos_x,node.pos_y),RADIUS,0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
 
-
-for node in graph.nodes:
-    print("Node id: "+ str(node.id) + "\nX,Y:" + str(node.pos_x) + "," + str(node.pos_y) + "\nConnected Nodes:" + str(node.connected_nodes))
-
+pygame.quit()
