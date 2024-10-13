e88fb71568d135dafcc02bb330637098adec018b
ardacbk <ardacabuk1@hotmail.com>
2024-10-13 13:36:49 +0300

    Drawing the graph near complete

--- DFS/node.py
+++ DFS/node.py
@@ -1,6 +1,8 @@
 class Node:
-    def __init__(self, id, pos_x, pos_y,connected_nodes):
+    def __init__(self, id, pos_x, pos_y,connected_nodes=None):
         self.id = id
         self.pos_x = pos_x
         self.pos_y = pos_y
+        if connected_nodes == None:
+            connected_nodes = []
         self.connected_nodes = connected_nodes
